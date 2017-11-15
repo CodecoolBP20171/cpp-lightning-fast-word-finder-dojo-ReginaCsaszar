a2685f8be1ed315139acdaeb19396c8c95f96ed0
Laszlo Molnar <laszlo.molnar@codecool.com>
2017-11-15 16:40:05 +0100

    Dojo base repo

---
+++ LightningWordFinder.hpp
@@ -0,0 +1,15 @@
+//
+// Created by László Molnár on 2017-11-15.
+//
+
+#ifndef CPP_LIGHTNING_FAST_WORD_FINDER_DOJO_LIGHTNINGWORDFINDER_HPP
+#define CPP_LIGHTNING_FAST_WORD_FINDER_DOJO_LIGHTNINGWORDFINDER_HPP
+class LightningWordFinder
+{
+public:
+    std::string getLongestWord(const std::string& text)
+    {
+        return "Longest Word";
+    }
+};
+#endif //CPP_LIGHTNING_FAST_WORD_FINDER_DOJO_LIGHTNINGWORDFINDER_HPP
